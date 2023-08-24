57116cef63980684b6e9881fd1131df04b29f790
Jemini Willis <jdubdev@pm.me>
2023-08-23 23:49:06 -0500

    feat: scrolling direction, zooming, multi monitor support
    Added Direction (Vert/Horz)
    Fixed Zooming
    Fixed Multi Monitor Mouse Clicking

--- Code/ControlModes/ButtonPressControl.swift
+++ Code/ControlModes/ButtonPressControl.swift
@@ -37,16 +37,42 @@ class ButtonPressControl: DeviceControl {
     }
 
     private func sendMouse(eventType: CGEventType) {
-        let mousePos = NSEvent.mouseLocation
-        let screenHeight = NSScreen.main?.frame.height ?? 0
-        let translatedMousePos = NSPoint(x: mousePos.x, y: screenHeight - mousePos.y)
-        let event = CGEvent(mouseEventSource: nil, mouseType: eventType, mouseCursorPosition: translatedMousePos, mouseButton: .left)
+        let mouseOriginal = NSEvent.mouseLocation
+        var mouseLocation = NSEvent.mouseLocation
+        
+        let screens = NSScreen.screens;
+        
+        //Multi monitor setups
+        
+        //NSScreen.main is the focused display NOT the Menu Bar display
+        //let screenMain = NSScreen.main;
+        
+        //Anyway, if what you want is the primary display, the one at (0, 0), just use NSScreen.screens[0].
+        let screenMain = NSScreen.screens[0];
+        
+        let screenWithMouse = (screens.first { NSMouseInRect(mouseLocation, $0.frame, false) })
+        
+        let frameFromMain = screenMain.frame
+        let frameFromScreen = screenWithMouse!.frame
+        
+        //X is a straight translation from the farthest left screen starting at 0
+        //mouseLocation.x -= frameFromScreen.origin.x;
+        
+        //Expected Y input is the hight of the Primary (menu bar) Display minus recieved Y location
+        //Anything else and it breaks multi monitor setups or makes the cursor jump everywhere
+        mouseLocation.y = frameFromMain.height - mouseLocation.y
+        
+        let event = CGEvent(
+            mouseEventSource: nil,
+            mouseType: eventType,
+            mouseCursorPosition: mouseLocation,
+            mouseButton: .left
+        )
+        
         event?.post(tap: .cghidEventTap)
-
-        log(tag: "Scroll", "sent mouse event: \(eventType.rawValue)")
     }
 
-    func rotationChanged(_ rotation: RotationState) -> Bool {
+    func rotationChanged(_ rotation: RotationState, _ axis: ScrollDirection) -> Bool {
         return false
     }
 }
